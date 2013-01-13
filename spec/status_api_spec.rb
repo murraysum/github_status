f7b917e8f6b9dc026ce56c4eb0f18fcf45b37f4e
Murray Summers <murray.sum@gmail.com>
2013-01-13 22:24:10 +0000

    Add spec to test querying the last message endpoint.

--- spec/status_api_spec.rb
+++ spec/status_api_spec.rb
@@ -39,5 +39,44 @@ describe Github::StatusAPI do
       end
     end
   end
+
+  describe "#last_message" do
+    before do
+      @expected_response = {
+        status: "good",
+        body: "good status message",
+        created_on: Time.now.to_s
+      }
+
+      @stub_request = stub_request(:get, "https://status.github.com/api/last-message.json")
+      @stub_request.to_return(body: JSON.generate(@expected_response))
+
+      @actual_response = Github::StatusAPI.last_message
+    end
+
+    describe "request" do  
+      it "should have been requested" do
+        @stub_request.should have_been_requested
+      end
+
+      it "should have requested last-message.json" do
+        @stub_request.should have_requested(:get, "https://status.github.com/api/last-message.json")
+      end
+    end
+
+    describe "response" do
+      it "should have responded with a status" do
+        @actual_response[:status].should == @expected_response[:status] 
+      end
+
+      it "should have responded with a body" do
+        @actual_response[:body].should == @expected_response[:body] 
+      end
+
+      it "should have responded with a created_on" do
+        @actual_response[:created_on].should == @expected_response[:created_on] 
+      end
+    end
+  end
 end
 
